4d4e9c0bf4e0a43d7e3eed1b0191091e823d68b5
Eino Kubo <eino.near@gmail.com>
2017-11-11 11:08:03 +0000

    using my function block.

--- howManyAlphabet.cc
+++ howManyAlphabet.cc
@@ -2,30 +2,41 @@
 #include <string.h>
 #include <vector>
 #include <iostream>
+#include "howManyAlphabet.h"
 using namespace std;
 
+
 int main (void) {
-	vector<int> counter(26, 0);
+	//vector<int> counter(26, 0);
 	string input;
 	cout << "文字列に含まれているアルファベットの個数をカウントします >> ";
 	getline(cin, input);
 	
-	int cnt = input.size();
+	//int cnt = input.size();
+	string strs = "abcdefghijklmnopqrstuvwxyz1234567890 !?\"#$%&'(){}[]=~|-^\\`+*<>_@;:,.";
+	vector<int> counter;
+	counter = howMany(input,strs);
+	for (int i = 0;i<counter.size();i++) {
+		cout << strs[i] << ":" << counter[i] << "\n";
+	}
 
-	char alphabet ='a';
-	for (int i=0;i<cnt;i++) {
-		alphabet = 'a';
-		for (int j=0;j<26;j++) {
-			if(input[i]==alphabet)
-				++counter[j];
-			++alphabet;
+	return 0;
+}
+
+vector<int> howMany(string searchSource,string strs) {
+	// string strs = "abcdefghijklmnopqrstuvwxyz1234567890";
+	vector<int> cnt(strs.size(),0);
+	
+	for (auto i = 0;i<searchSource.size();i++) {
+		for (auto j = 0;j<strs.size();j++) {
+			if (searchSource[i]==strs[j]) {
+				++cnt[j];
+				// cout << "hello";
+			}
+			// cout << j << ":" << cnt[j] << "\n";
 		}
 	}
 	
-	alphabet = 'a';	
-	for (int i=0;i<26;i++) {
-	    printf("%cの個数:%d\n",alphabet,counter[i]);
-		++alphabet;
-	}
-	return 0;
+	
+	return cnt;
 }
---
+++ howManyAlphabet.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> howMany(string searchSource,string strs);
